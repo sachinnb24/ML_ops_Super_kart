ad00db8bd760ecf3eda26094a626d0cd077889f3
sachinnb24 <sachinknv96@gmail.com>
2026-05-10 18:36:04 +0530

    Initial commit: SuperKart MLOps pipeline

--- superkart_project/deployment/app.py
+++ superkart_project/deployment/app.py
@@ -4,6 +4,7 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 
+
 # Load the saved model from Hugging Face Model Hub
 model_path = hf_hub_download(
     repo_id="Sachinnb24/Super-kart-predition",
